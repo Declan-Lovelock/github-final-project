7f044d77c4724402246d82f77190059035d684e1
Declan <56608839+Declan-Lovelock@users.noreply.github.com>
2023-06-13 16:37:44 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Declan-Lovelock
    # Input:
    # p, principal amount
    # t, time period in years
